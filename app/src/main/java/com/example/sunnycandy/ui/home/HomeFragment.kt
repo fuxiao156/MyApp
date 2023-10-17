6afa7eede2c175b9755c67facdb46fdb763684e5
Zency <2659255665@qq.com>
2023-10-17 20:42:33 +0800

    排列了盒子，并且实现了居中

--- app/src/main/java/com/example/sunnycandy/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sunnycandy/ui/home/HomeFragment.kt
@@ -1,15 +1,12 @@
 package com.example.sunnycandy.ui.home
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.sunnycandy.databinding.FragmentHomeBinding
-import com.example.sunnycandy.utils.ImageUtils
 
 class HomeFragment : Fragment() {
 
@@ -33,17 +30,25 @@ class HomeFragment : Fragment() {
 
 
 //        处理图片
-        val textView2 : TextView = binding.icon1
-        textView2.text = "sunnycandy"
-        val drawable = textView2.compoundDrawables
-        val context: Context = requireContext()
-        val scaledDrawable = ImageUtils.scaleDrawable(drawable[1], targetWidth = 180)
-        textView2.setCompoundDrawablesWithIntrinsicBounds(
-            null,
-            scaledDrawable, // top，这里设置缩放后的 Drawable
-            null,
-            null,
-        )
+//        val gridLayout: GridLayout = binding.gridLayout
+//        val gridLen = gridLayout.childCount
+//        for(i in 0 until gridLen)
+//        {
+//            val textView2 = gridLayout.getChildAt(i)
+//            if(textView2 is TextView)
+//            {
+//                textView2.text = "sunnycandy"
+//                val drawable = textView2.compoundDrawables
+//                val context: Context = requireContext()
+//                val scaledDrawable = ImageUtils.scaleDrawable(drawable[1], targetWidth = 180)
+//                textView2.setCompoundDrawablesWithIntrinsicBounds(
+//                    null,
+//                    scaledDrawable, // top，这里设置缩放后的 Drawable
+//                    null,
+//                    null,
+//                )
+//            }
+//        }
         return root
     }
 
