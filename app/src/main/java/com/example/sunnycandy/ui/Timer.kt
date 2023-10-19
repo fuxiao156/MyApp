709426ccd907f68f33ccc529c6f24cfbc60029a6
Zency <2659255665@qq.com>
2023-10-19 18:11:03 +0800

    加上了通知

--- app/src/main/java/com/example/sunnycandy/ui/Timer.kt
+++ app/src/main/java/com/example/sunnycandy/ui/Timer.kt
@@ -1,8 +1,13 @@
 package com.example.sunnycandy.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.sunnycandy.databinding.ActivityTimerBinding
+import com.example.sunnycandy.utils.notificationUtils
 
 class Timer : AppCompatActivity() {
 
@@ -21,5 +26,22 @@ class Timer : AppCompatActivity() {
             val minite = binding.minite.text.toString().toInt()
             val second = binding.second.text.toString().toInt()
         }
+        val notification = notificationUtils(this)
+        // 创建通知渠道
+        notification.createNotificationChannel("timer", "timer")
+        // 发送通知
+        val title = "通知标题"
+        val contentText = "通知内容"
+        val permission = Manifest.permission.POST_NOTIFICATIONS
+        val granted = PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, permission)
+        if (granted) {
+            // 已授权，执行需要权限的操作
+            notification.sendNotification("timer", 2,  title, contentText)
+        } else {
+            // 未授权，需要请求权限或执行适当的处理
+            ActivityCompat.requestPermissions(this, arrayOf(permission), 100)
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/sunnycandy/utils/notificationUtils.kt
@@ -0,0 +1,51 @@
+package com.example.sunnycandy.utils
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.example.sunnycandy.R
+
+class notificationUtils (private val context: Context){
+    // 创建通知渠道
+    fun createNotificationChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance)
+
+            val notificationManager =
+                context.getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+    fun sendNotification(
+        channelId: String,
+        notificationId: Int,
+        title: String,
+        contentText: String,
+    ) {
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setContentTitle(title)
+            .setContentText(contentText)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+//            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        // 发送通知
+        val notificationManager = NotificationManagerCompat.from(context)
+        val permission = Manifest.permission.POST_NOTIFICATIONS
+        val granted = PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, permission)
+        if (granted) {
+            // 已授权，执行需要权限的操作
+            notificationManager.notify(notificationId, builder.build())
+//            Log.d("Timer","已获得授权")
+        }
+        else{
+            Log.d("Timer","未获得授权")
+        }
+    }
+}
