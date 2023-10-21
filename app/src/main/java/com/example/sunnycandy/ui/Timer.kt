b75f933a5c62a9f0f701d290baf7e7d1ece3e493
Zency <2659255665@qq.com>
2023-10-21 14:30:01 +0800

    优化了添加item的过程

--- app/src/main/java/com/example/sunnycandy/ui/Timer.kt
+++ app/src/main/java/com/example/sunnycandy/ui/Timer.kt
@@ -4,10 +4,8 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -21,7 +19,6 @@ import java.util.concurrent.TimeUnit
 import androidx.work.Data
 import androidx.work.ExistingWorkPolicy
 import com.example.sunnycandy.R
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 
@@ -33,6 +30,8 @@ class Timer : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityTimerBinding.inflate(layoutInflater)
+        // 获取LayoutInflater
+        val inflater = LayoutInflater.from(this)
         setContentView(binding.root)
 
         // 在Activity或Fragment中请求后台权限
@@ -56,23 +55,35 @@ class Timer : AppCompatActivity() {
         data class HistoryItem(val title:String, val content:String,val numInterval: Int, val interval: String,  val startTime: String)
         // 获取 SharedPreferences 中的 JSON 字符串
         val historySharedPred = this.getSharedPreferences("historyItems", Context.MODE_PRIVATE)
+        val editor = historySharedPred.edit()
         val historyJson = historySharedPred.getString("historyData","[]")
         // 将 JSON 字符串转换为对象列表
         val gson = GsonBuilder().create()
         val historyItems: MutableList<HistoryItem> = gson.fromJson(historyJson, object : TypeToken<MutableList<HistoryItem>>(){}.type)
-        Log.d("获取Items，为",historyJson.toString())
-        Log.d("获取Items非Json格式为",historyItems.toString())
-
         //获取历史闹钟列表元素
         val history = binding.history
 
-        // 获取LayoutInflater
-        val inflater = LayoutInflater.from(this)
+        //将对象列表转化为视图
+        for (item in historyItems){
+            val clockItem = inflater.inflate(R.layout.clock_item,null)
+            clockItem.findViewById<TextView>(R.id.itemtitle).text = item.title
+            clockItem.findViewById<TextView>(R.id.itemcontent).text = item.content
+            clockItem.findViewById<ImageView>(R.id.cancelClock).setOnClickListener{
+                history.removeView(clockItem)
+                WorkManager.getInstance(this).cancelUniqueWork(item.title)
+                historyItems.remove(item)
+                val tempJson = gson.toJson(historyItems)
+                editor.putString("historyData",tempJson).apply()
+            }
+            history.addView(clockItem)
+        }
+
+
         //给按钮绑定点击事件
         button.setOnClickListener{
-//            val day = binding.day.text.toString()
-//            val hour = binding.hour.text.toString()
-//            val minite = binding.minite.text.toString()
+            val day = binding.day.text.toString()
+            val hour = binding.hour.text.toString()
+            val minite = binding.minite.text.toString()
             //绑定通知内容
             val title = binding.title.text.toString()
             val content = binding.content.text.toString()
@@ -106,42 +117,28 @@ class Timer : AppCompatActivity() {
                 WorkManager.getInstance(this).enqueueUniqueWork(title,
                     ExistingWorkPolicy.REPLACE,workRequest)
 
+                //创建新的 HistoryItem对象
+                val newItem = HistoryItem(title,content,10,"10秒","now")
+                historyItems.add(newItem)
+                //保存对象到SharedPreference
+                val tempJson = gson.toJson(historyItems)
+                editor.putString("historyData",tempJson).apply()
+                //创建对应的视图
                 val clockItem = inflater.inflate(R.layout.clock_item,null)
                 clockItem.findViewById<TextView>(R.id.itemtitle).text = title
                 clockItem.findViewById<TextView>(R.id.itemcontent).text = content
                 clockItem.findViewById<ImageView>(R.id.cancelClock).setOnClickListener{
                     history.removeView(clockItem)
                     WorkManager.getInstance(this).cancelUniqueWork(title)
+                    historyItems.remove(newItem)
+                    val tempJson = gson.toJson(historyItems)
+                    editor.putString("historyData",tempJson).apply()
                 }
                 history.addView(clockItem)
-
-                //创建新的 HistoryItem对象
-                val newItem = HistoryItem(title,content,10,"10秒","now")
-                historyItems.add(newItem)
-                //保存对象到SharedPreference
-                val tempJson = gson.toJson(historyItems)
-//                val editor = historySharedPred.edit()
-//                editor.putString("historyData",tempJson).apply()
-                Log.d("newItem的值是",newItem.toString())
-                Log.d("nweItem的json是",gson.toJson(newItem))
-                Log.d("historyItem的值是",historyItems.toString())
-                Log.d("保存data,items的值为",tempJson.toString())
             } else {
                 // 未授权，需要请求权限或执行适当的处理
                 ActivityCompat.requestPermissions(this, arrayOf(permission), 100)
             }
         }
-
-        val cancelButton = binding.cancel
-        cancelButton.setOnClickListener {
-            WorkManager.getInstance(this).cancelAllWorkByTag("test")
-        }
-        val cancelButton2 = binding.cancel2
-        cancelButton2.setOnClickListener {
-            WorkManager.getInstance(this).cancelUniqueWork(binding.title.text.toString())
-        }
-        val testButton = binding.test
-        val editor = historySharedPred.edit()
-        editor.putString("historyData","[]").apply()
 }
 }
